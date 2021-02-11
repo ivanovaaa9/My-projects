aee2035672f32cd1e2578d78ed5bb878c8dc0b75
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-11 10:33:15 +0200

    Add files via upload

---
+++ Counter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0, num1, sum;
+
+            Console.Write("Enter a number: ");
+            num1 = int.Parse(Console.ReadLine());
+            while (counter <= 10)
+            {
+                sum = num1 * counter;
+
+                Console.WriteLine("{0} x {1}= {2} ", num1, counter, sum);
+                counter++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
