a27174fe839d9217a379b77f003e5eaf9372d54c
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-11 11:38:30 +0200

    Add files via upload

---
+++ Calculate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 2;
+            int z = 3;
+            int value1 = (x + y) * z;
+            int value2 = x * y + y * z;
+            Console.WriteLine("Result: (x + y)*z = " + value1
+                + ", x*y + y*z = " + value2);
+            Console.ReadKey();
+
+        }
+    }
+}
