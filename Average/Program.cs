5b6cee9d668e16560e6e349c8b36441308e95e55
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-11 10:38:30 +0200

    Add files via upload

---
+++ Average/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int number_1 = 10;
+            int number_2 = 15;
+            int number_3 = 25;
+            int number_4 = 10;
+
+            int avg = (number_1 + number_2 + number_3 + number_4) / 4;
+            Console.WriteLine("Result: average" +
+                " value = " + avg);
+            Console.ReadKey();
+
+        }
+    }
+}
