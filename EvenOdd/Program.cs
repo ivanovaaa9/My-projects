09a1c77f4ebc1a8b1207c07f565d5867cb6a4b47
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-12 11:29:41 +0200

    Add files via upload

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Number: " + number);
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+
+                Console.WriteLine("odd");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
