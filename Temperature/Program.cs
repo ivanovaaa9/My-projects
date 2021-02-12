dceb027d7e8e7387bf3ba2682bf84182cd998fa4
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-12 11:33:04 +0200

    Add files via upload

---
+++ Temperature/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value in Celsius:");
+            int c = int.Parse(Console.ReadLine());
+            int k = (c + 273);
+            int f = ((c * 18) / 10 + 32);
+            Console.WriteLine("In Kelvin: " + k + ", in Fahrenheit: "
+                + f);
+            Console.ReadKey();
+
+        }
+    }
+}
