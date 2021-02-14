78d0df2d5e171c723147fbcffbdaf7efc2192ae6
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-14 11:00:53 +0200

    Add files via upload

---
+++ SurfaceVolume/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius:");
+            int radius = int.Parse(Console.ReadLine());
+
+            double surface = 4 * Math.PI * radius * radius;
+            double volume = (3 * Math.PI * radius * radius * radius) / 4;
+            Console.WriteLine("Surface: " + surface);
+            Console.WriteLine("Volume: " + volume);
+            Console.ReadKey();
+
+        }
+    }
+}
