b43689831127de4d0341c99abda0db604f666f55
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-14 10:57:33 +0200

    Add files via upload

---
+++ CircleMathPI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius: ");
+            int radius = int.Parse(Console.ReadLine());
+
+            double parameterCircle = Math.PI * radius * 2;
+            double areaCircle = Math.PI * radius * radius;
+
+            Console.WriteLine("The parameter of the circle is: " + parameterCircle +
+                ", the area of the circle is: " + areaCircle);
+            Console.ReadKey();
+
+        }
+    }
+}
