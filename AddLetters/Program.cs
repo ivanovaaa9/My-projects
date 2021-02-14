7c471946195db2f850b3bbd5d58614988a503ffd
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-14 10:52:54 +0200

    Add files via upload

---
+++ AddLetters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter_1, letter_2, letter_3;
+            letter_1 = char.Parse(Console.ReadLine());
+
+            letter_2 = char.Parse(Console.ReadLine());
+            letter_3 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("letter_1: " + letter_3 + " ,letter_2: " + letter_2 + ",letter_3: " + letter_1);
+            Console.ReadKey();
+
+        }
+    }
+}
