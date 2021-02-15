83c8664f627cb1d4ab5762abae5d7849e2feb56c
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-15 09:14:29 +0200

    Add files via upload

---
+++ IsLetterOr/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symb;
+
+            Console.Write("Input a symbol: ");
+            symb = char.Parse(Console.ReadLine());
+
+            if ((symb == 'a') || (symb == 'e') || (symb == 'i') ||
+                    (symb == 'o') || (symb == 'u'))
+            {
+                Console.WriteLine("It's a vowel.");
+            }
+            else if ((symb >= '0') && (symb <= '9'))
+            {
+                Console.WriteLine("It's a digit.");
+            }
+            else
+            {
+                Console.WriteLine("It's another symbol.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+    
+
