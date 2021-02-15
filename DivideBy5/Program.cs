adf0ce1631a360fcde0e05c1e8ccc10686e8acb3
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-15 09:02:02 +0200

    Add files via upload

---
+++ DivideBy5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            if (num1 % 5 == 0)
+            {
+                Console.WriteLine("Number " + num1 + " is divisible by 5.");
+
+            }
+            else
+            {
+                Console.WriteLine("Number " + num1 + " is not divisible by 5.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
