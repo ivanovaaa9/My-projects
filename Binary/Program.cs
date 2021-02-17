600ccac6afa3a2125b721a96f3c1b468bcf3b2c7
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-17 22:49:13 +0200

    Add files via upload

---
+++ Binary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string decimal1;
+            string binary1;
+
+            Console.WriteLine("Input a Number : ");
+            decimal1 = Console.ReadLine();
+
+            int num = Convert.ToInt32(decimal1);
+            binary1 = "";
+            while (num > 1)
+            {
+                int remainder = num % 2;
+                binary1 = Convert.ToString(remainder) + binary1;
+                num /= 2;
+            }
+            binary1= Convert.ToString(num) + binary1;
+            Console.WriteLine("Binary: {0}", binary1);
+            Console.ReadKey();
+        }
+
+    }
+}
+
