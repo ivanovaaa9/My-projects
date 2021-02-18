4b2e77e38f0f70382d71500bfb6e903eb922e47e
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-18 09:38:32 +0200

    Add files via upload

---
+++ For/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number_1 =int.Parse(Console.ReadLine());
+            Console.WriteLine("Number: " + number_1);
+
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(number_1 * i);
+                
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
