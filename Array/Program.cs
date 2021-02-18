26abb238c561869ef1565bdc3c5774e85a20f728
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-18 09:39:22 +0200

    Add files via upload

---
+++ Array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10] { 2, 4, 6, 8, 10, 12, 14, 16,18,20};
+          
+            Console.WriteLine("Elements in array are: ");
+            for(int i = 0; i <10; i++)
+            {
+                Console.WriteLine("{0}", array[i]);
+            }
+            Console.Write(" ");
+            Console.ReadKey();
+            
+        }
+        
+    }
+}
