a8329120ed4a278f79fcc9320d5e31647f28312c
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-19 13:54:03 +0200

    Add files via upload

---
+++ ArraySameLenght/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[2] { 1, 2 };
+            int[] array2 = new int[2] { 3, 2 };
+            Console.WriteLine(" Length of a first array = 2, first element = 1 and second element =2");
+            Console.WriteLine(" Length of a second array =2, first element= 3, second element = 2. ");
+
+            if(array == array2)
+            {
+                Console.WriteLine("Arrays are the same with the same lenght.");
+
+            }
+            else
+            {
+                Console.WriteLine("The arrays are not equal but they have the same length.");
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
