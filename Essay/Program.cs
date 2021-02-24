c53e86c26d3b0baf5abb0f2cb8247f951f5b711a
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-24 08:02:10 +0200

    Add files via upload

---
+++ Essay/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "In my essay I will share my personal story about memories from the last summer, how this program changes my life as well as my why I want to do it again.Last year, I have heard about student exchange programs to work and travell in America and this grabbed my attention. I decided to try. With comming in America my first impressions were how kind and responsive were people everywhere.Going in America gave me  the courage that I can do a lot of things just in 1 summer. travelling, visiting an incredible places in the state where I stayed, meeting amazing people, creating long lasting friendships.I felt the beauty in different languages, learned new words in spanish, romanian, bangla as well as improving my English skills.It was very nice to learn about the other participants culture, also to see how americans work, to meet more professionals from the area where I worked. ";
+            
+            Console.WriteLine("Test data:" + a);
+            
+            Console.WriteLine(a.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
