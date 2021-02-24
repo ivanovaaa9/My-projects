7e1d9270f32baed330f5687c413bfc966283b27a
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-24 08:13:45 +0200

    Add files via upload

---
+++ WordLength/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = Console.ReadLine().ToLower().Replace(" ","");
+            int consonants = 0;
+            int vowels = 0;
+            int space = 0;
+            for(int i = 0; i < words.Length; i++)
+            {
+                if (words[i] == 'a' || words[i] == 'e' || words[i] == 'i' || words[i] == 'o' || words[i] == 'u')
+                {
+                    vowels++;
+                
+                }else{
+                    consonants++;
+                }
+
+            }
+            Console.WriteLine("Total number of vowels:{0} ", vowels);
+            Console.WriteLine("Total number of consonatnts:{0} ", consonants);
+            Console.ReadKey();
+
+        }
+          
+        }
+    }
+
