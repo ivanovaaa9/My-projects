8e141537a24c7c5aeb012c0647107ecf7cfd1f7e
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-24 08:08:38 +0200

    Add files via upload

---
+++ DateTime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime datet = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("{0}", datet.Year);
+            Console.WriteLine("{0}", datet.Month);
+            Console.WriteLine("{0}", datet.Day);
+            Console.WriteLine("{0}", datet.Hour);
+            Console.WriteLine("{0}", datet.Minute);
+            Console.WriteLine("{0}", datet.Second);
+            Console.ReadKey();
+
+        }
+       
+       
+    }
+}
