dd78cd5ba64e06dfd5dffe6c8e388c06cae89c6f
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-24 08:16:44 +0200

    Add files via upload

---
+++ NameOfTheDay/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            
+            DateTime date = new DateTime(2017, 3, 3);
+            Console.WriteLine("day=" + year + ", month = 3" + ", year= 2017");
+            Console.WriteLine(date.DayOfWeek);
+            
+            
+
+            Console.ReadKey();
+        }
+        
+
+    }
+    }
+
