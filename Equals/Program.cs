d3e60376ca9d28712ca3b6d4121b43f1da8dff96
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-24 08:10:28 +0200

    Add files via upload

---
+++ Equals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence1 = Console.ReadLine();
+            string sentence2 = Console.ReadLine();
+            if (sentence1.Equals(sentence2))
+            {
+                Console.WriteLine("They are the same.");
+            }
+            else
+            {
+                Console.WriteLine("The sentences are not equal");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
