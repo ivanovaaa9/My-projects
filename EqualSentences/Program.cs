300f29f5a25347fcab199248f66c47c8501a49ce
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-26 10:48:22 +0200

    Add files via upload

---
+++ EqualSentences/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String sentence1 = Console.ReadLine();
+            String sentence2 = Console.ReadLine();
+            bool areEqual = true;
+            if(sentence1.Length != sentence2.Length)
+            {
+                areEqual = false;
+            } else
+            {
+                areEqual = sentence1.CompareTo(sentence2) == 0;
+            }
+
+            if(areEqual)
+            {
+                Console.WriteLine("The sentences are equal.");
+            } else
+            {
+                Console.WriteLine("The sentences are not equal.");
+            }
+        }
+    }
+}
