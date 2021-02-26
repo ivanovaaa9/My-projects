96270ae5d4721b8efa64eab73bd77a3e1f0f1f25
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-26 10:57:26 +0200

    Add files via upload

---
+++ DateMonthYera/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Day = ");
+            int day = int.Parse(Console.ReadLine());
+            Console.Write("Month = ");
+            int month = int.Parse(Console.ReadLine());
+            Console.Write("Year = ");
+            int year = int.Parse(Console.ReadLine());
+            DateTime givenDate = new DateTime(year, month, day);
+            Console.WriteLine("Result: {0}", givenDate.DayOfWeek);
+        }
+    }
+}
