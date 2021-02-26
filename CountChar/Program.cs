7f34655832d9cc8d263153b13d2204a8b61b1393
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-26 10:44:56 +0200

    Add files via upload

---
+++ CountChar/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Test data: ");
+            Console.WriteLine("Result: {0}", Console.ReadLine().Length);
+        }
+    }
+}
