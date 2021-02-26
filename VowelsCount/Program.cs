ff43d62824c2dc152772f8c108a78453debd5bf9
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-26 10:55:35 +0200

    Add files via upload

---
+++ VowelsCount/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            String vowels = "aeiou";
+
+            int vowelsCount = input.ToCharArray().Count(c => vowels.IndexOf(c.ToString(), 0) >= 0);
+            Console.WriteLine("Vowels Count: {0}", vowelsCount);
+            Console.WriteLine("Consonants Count: {0}", input.Replace(" ", string.Empty).Length - vowelsCount);
+        }
+    }
+}
