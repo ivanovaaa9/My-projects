1f3d99ad17620fcf3151dad1ce404210e40adb6a
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-02-28 23:54:33 +0200

    Add files via upload

---
+++ SentencesReverse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sentence:");
+           string letters = Console.ReadLine();
+            string[] sentence = letters.Split(' ');
+            int new1 = sentence.Length - 1;
+            
+            
+            Console.WriteLine("The sentence in reverse order:");
+            for(int i = letters.Length-1; i >= 0; i--)
+            {
+                Console.Write(sentence[new1] + "" + ' ');
+                new1--;
+                
+            }
+            Console.ReadKey();
+        }
+           
+                
+        }
+    }
+    
+
