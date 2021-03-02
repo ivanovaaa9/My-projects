d93de41bee022003e7b14d0d06eecbfe8788d23f
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-02 23:10:10 +0200

    Add files via upload

---
+++ BookstoreReverseNames1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3New
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Matrix size = ");
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+            Console.Write("From = ");
+            int from = int.Parse(Console.ReadLine());
+            Console.Write("To = ");
+            int to = int.Parse(Console.ReadLine());
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[j, i] = from++;
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.WriteLine();
+                Console.ReadKey();
+
+            }
+
+
+        }
+    }
+}
+
