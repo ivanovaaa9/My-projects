c811a4e0d9a9b8116a6ff726e91e5ea061ae7af0
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-02 23:08:51 +0200

    Add files via upload

---
+++ CountChar1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter input: ");
+            char[] strippedChars = Console
+                .ReadLine().ToCharArray().Where(c => Char.IsLetter(c) || c == ' ').ToArray();
+            Stack<String> wordsStack = new Stack<String>(new string(strippedChars).Split(' '));
+            int wordsCount = wordsStack.Count;
+            for (int i = 0; i < wordsCount; i++)
+            {
+                string poped = wordsStack.Pop();
+                Console.Write(poped + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
