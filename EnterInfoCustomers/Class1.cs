b63d85dbcd061b84723be231914cfca8c143b9f1
ivanovaaa9 <50923185+ivanovaaa9@users.noreply.github.com>
2021-03-02 23:15:06 +0200

    Add files via upload
    Write a C# program for entering information about the customers of a given organization. Use a class with appropriate attributes for storing names, addresses and phone numbers. Then print in the console the stored information. Try to organize the code in two files.

---
+++ EnterInfoCustomers/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app6
+{
+    class client
+    {
+        String name;
+        String address;
+        String pnumber;
+        
+        public string Name
+        {
+           set { name = value; }
+           get { return name; }
+
+        }
+
+        public string Address
+        {
+           set { address = value; }
+           get { return address; }
+        }
+
+        public string Pnumber
+        {
+            set { pnumber = value; }
+            get { return pnumber;  }
+
+        }
+        
+       
+        }
+    }
+
+
+
---
+++ EnterInfoCustomers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app6
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+            client client1 = new client();
+            Console.WriteLine("Enter a customer name: ");
+            client1.Name=Console.ReadLine();
+            Console.WriteLine("Enter a customer address: ");
+            client1.Address=Console.ReadLine();
+            Console.WriteLine("Enter a customer number: ");
+            client1.Pnumber=Console.ReadLine();
+
+            Console.WriteLine("Customer name:" + client1.Name + " ;Customer address:" + client1.Address + " ;Customer phone number:" + client1.Pnumber);
+          
+
+
+
+
+
+
+
+
+        }
+       
+        }
+    }
+
